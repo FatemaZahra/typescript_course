87e99a4c0c84cb5c643c5f227e232bab3ef4f7c3
Fatema Zahra <fatema.zahra1312@gmail.com>
2023-08-25 09:40:27 +0100

    lesson_4

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,29 @@
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
-//character = 20;
-character = "luigi";
-//age = "yoshi"
-age = 40;
-//isBlackBelt = "yes"
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//Arrays
+var names = ["luigi", "mario", "yashi"];
+//names = "hello";
+names.push("toad");
+//names.push(3);
+//names[0] = 3;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push("me");
+// numbers[1] = "me";
+var mixed = ["ken", 4, "chun-li", 8, 9];
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+//Objects
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 40,
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = "ryu";
+//ninja.age = "50";
+//ninja.skills = ["fighting", "sneaking"];
+ninja = {
+    name: "yoshi",
+    belt: "orange",
+    age: 40,
+};
