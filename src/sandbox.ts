1e86ad73a21aa8b93028c4799550addc8696f555
Fatema Zahra <fatema.zahra1312@gmail.com>
2023-08-28 19:13:19 +0100

    function signature

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,13 +1,26 @@
-type StringorNum = string | number;
-type objWithName = { name: string; uid: StringorNum };
+//let greet: Function;
 
-const logDetails = (uid: StringorNum, item: string) => {
-  console.log(`${item} has a uid of ${uid}`);
-};
-const greet = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+//example 1
+let greet: (a: string, b: string) => void;
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 };
 
-const greetAgain = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+//example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === "add") {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+};
+
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type person = { name: string; age: number };
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
 };
