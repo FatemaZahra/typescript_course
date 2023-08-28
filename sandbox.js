96ed96d8dae14ff845ccb4f557bb8e454c632622
Fatema Zahra <fatema.zahra1312@gmail.com>
2023-08-28 15:44:14 +0100

    lesson 6

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,15 @@
-//explicit types
-var character;
-var age;
-var isLoggedIn;
-//age = "luigi";
-age = 35;
-//isLoggedIn = 34;
-isLoggedIn = true;
-//Arrays
-var ninjas = []; //to be able to push
-ninjas = ["10", "20"];
-ninjas.push("shaun");
-//Union types
+var age = 25;
+age = true;
+console.log(age);
+age = "twenty-five";
+console.log(age);
+age = { name: "luigi" };
+console.log(age);
 var mixed = [];
-mixed.push("hello");
-mixed.push(20);
+mixed.push("mario");
+mixed.push(5);
 mixed.push(false);
 console.log(mixed);
-var uid;
-uid = 123;
-uid = "123";
-//Objects
-var ninjaOne;
-ninjaOne = { name: "fama", age: 25 };
-ninjaOne = [];
-var ninjaTwo;
+var ninja;
+ninja = { name: "yoshi", age: 25 };
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,33 +1,17 @@
-//explicit types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any = 25;
+age = true;
+console.log(age);
+age = "twenty-five";
+console.log(age);
+age = { name: "luigi" };
+console.log(age);
 
-//age = "luigi";
-age = 35;
-
-//isLoggedIn = 34;
-isLoggedIn = true;
-
-//Arrays
-let ninjas: string[] = []; //to be able to push
-ninjas = ["10", "20"];
-ninjas.push("shaun");
-
-//Union types
-let mixed: (string | number | boolean)[] = [];
-mixed.push("hello");
-mixed.push(20);
+let mixed: any[] = [];
+mixed.push("mario");
+mixed.push(5);
 mixed.push(false);
 console.log(mixed);
 
-let uid: string | number;
-uid = 123;
-uid = "123";
-
-//Objects
-let ninjaOne: object;
-ninjaOne = { name: "fama", age: 25 };
-ninjaOne = [];
-
-let ninjaTwo: {};
+let ninja: { name: any; age: any };
+ninja = { name: "yoshi", age: 25 };
+console.log(ninja);
